48651bff5ba02c4c639142f0902c9f703d15f101
ChoYoungHo <choyoungho95@gmail.com>
2020-12-10 15:57:21 +0900

    bodyparser, cookieparser, helmet, morgan 미들웨어

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import express from "express";
-const app = express();
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
+const app = express();
 const PORT = 4000;
 
 const handleHome = (req,res) =>{
     res.send('hello world');
-}
-
-const handleBetween = (req,res,next) => {
-    console.log('Im between');
-    next();
 }
 
 const handleProfile = (req,res) =>{
@@ -22,8 +21,11 @@ const handleListening = () =>{
 
 //route
 // app.get('/', handleBetween, handleHome);
-
-app.use(handleBetween);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
