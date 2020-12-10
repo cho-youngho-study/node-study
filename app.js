7e54caaebae2c032658991bb7d183a8b3f00e36a
ChoYoungHo <choyoungho95@gmail.com>
2020-12-10 16:15:21 +0900

    routing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
 
 const app = express();
-const PORT = 4000;
 
 const handleHome = (req,res) =>{
     res.send('hello world');
@@ -15,12 +15,7 @@ const handleProfile = (req,res) =>{
     res.send('profile page');
 }
 
-const handleListening = () =>{
-    console.log(`Listening Port: ${PORT}`);
-}
-
 //route
-// app.get('/', handleBetween, handleHome);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -30,5 +25,6 @@ app.use(morgan('dev'));
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
 
-//server
-app.listen(PORT, handleListening);
+app.use('/user', userRouter);
+
+export default app;
